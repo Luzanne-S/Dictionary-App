1ad2d8b5dd5d8d4b0e0456f809a19c378965c864
Luzanne <luzannejss@gmail.com>
2021-08-06 14:24:48 +0200

    Updated Dictionary.js
    Imported Axios and integrated Dictionary API

--- my-app/src/Dictionary.js
+++ my-app/src/Dictionary.js
@@ -1,11 +1,17 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(reponse) {
+    console.log(reponse.data[0]);
+  }
   function search(event) {
     event.preventDefault();
-    alert(`searching for ${keyword} definition`);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
