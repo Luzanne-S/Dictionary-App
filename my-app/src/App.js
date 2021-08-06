517e340316305fdb29438a2594b7b82a10523758
Luzanne <luzannejss@gmail.com>
2021-08-06 16:12:10 +0200

    Created an H1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <div className="container">
         <header className="App-header">
+          <h1>Dictionary</h1>
           <main>
             {" "}
             <Dictionary />
