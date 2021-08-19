2bae8b0b1defb2a1e4ddfef08c0984892718bc31
Luzanne <luzannejss@gmail.com>
2021-08-19 13:12:55 +0200

    Edited footer and updated css
    CSS: form and footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ function App() {
   return (
     <div className="App">
       <div className="container">
+        <div class="form-check form-switch switch">
+          <input
+            class="form-check-input"
+            type="checkbox"
+            id="flexSwitchCheckDefault"
+          />
+          <label class="form-check-label" for="flexSwitchCheckDefault"></label>
+        </div>
         <header className="App-header">
           <h1>Dictionary</h1>
           <main>
@@ -12,8 +20,11 @@ function App() {
             <Dictionary />
           </main>
         </header>
-        <footer className="project-footer"><a href="https://github.com/Luzanne-S/Dictionary-App">Coded by Luzanne</a></footer>
-        
+        <footer className="project-footer">
+          <a href="https://github.com/Luzanne-S/Dictionary-App">
+            Coded by Luzanne
+          </a>
+        </footer>
       </div>
     </div>
   );
