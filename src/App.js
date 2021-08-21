d6f46188a8d526abec291a4a813954fc21317d91
Luzanne <luzannejss@gmail.com>
2021-08-21 11:45:21 +0200

    Moved switch properties and styling to Index.html and Index.css

--- src/App.js
+++ src/App.js
@@ -5,14 +5,6 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <div class="form-check form-switch switch">
-          <input
-            class="form-check-input"
-            type="checkbox"
-            id="flexSwitchCheckDefault"
-          />
-          <label class="form-check-label" for="flexSwitchCheckDefault"></label>
-        </div>
         <header className="App-header">
           <h1>Dictionary</h1>
           <main>
